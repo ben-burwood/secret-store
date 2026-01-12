f25db7ad8ee8c534fff3f934840e0d3c426dbb79
Ben Burwood <ben.burwood@streetwave.co>
2026-01-12 20:05:53 +0000

    Api -> Web Distinction

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,7 @@ import './style.css';
 import "vue3-toastify/dist/index.css";
 import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
 
-export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api`;
+export const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/web`;
 
 createApp(App).use(
   Vue3Toastify, { theme: "colored", position: "bottom-right" } as ToastContainerOptions,
--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"net/http"
-	"secret-store/internal/api"
+	"secret-store/internal/web"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /secret/generate", api.GenerateSecretApi)
-	mux.HandleFunc("GET /secrets", api.ListSecretsApi)
-	mux.HandleFunc("POST /secrets/new", api.CreateSecretApi)
-	mux.HandleFunc("DELETE /secrets/{id}", api.DeleteSecretApi)
-	mux.HandleFunc("UPDATE /secrets/{id}", api.UpdateSecretApi)
-	mux.HandleFunc("GET /auth/key", api.GetAuthKeyApi)
-	mux.HandleFunc("GET /auth/key/generate", api.GenerateAuthKeyApi)
+	mux.HandleFunc("GET /web/secret/generate", web.GenerateSecretApi)
+	mux.HandleFunc("GET /web/secrets", web.ListSecretsApi)
+	mux.HandleFunc("POST /web/secrets/new", web.CreateSecretApi)
+	mux.HandleFunc("DELETE /web/secrets/{id}", web.DeleteSecretApi)
+	mux.HandleFunc("UPDATE /web/secrets/{id}", web.UpdateSecretApi)
+	mux.HandleFunc("GET /web/auth/key", web.GetAuthKeyApi)
+	mux.HandleFunc("GET /web/auth/key/generate", web.GenerateAuthKeyApi)
 
-	http.ListenAndServe("[::]:8080", api.CORSMiddleware(mux))
+	http.ListenAndServe("[::]:8080", web.CORSMiddleware(mux))
 }
